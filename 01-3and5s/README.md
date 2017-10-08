b197859817dfc73a4c00df0c51d5c6e3463148dd
Caleb <caleb.wells1@gmail.com>
2017-10-08 14:50:40 -0700

    first problem completed

---
+++ 01-3and5s/README.md
@@ -0,0 +1,3 @@
+# 3 and 5s
+
+If we evaluate all numbers that are multiples of 3 and 5 that are under 10, there is [3, 5, 6, 9], the sum is 23. Evaluate this for all multiples of 3 and 5 under 1,000.
---
+++ 01-3and5s/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let findMultiples = limit => {
+  let sum = 0
+  let increment = 0
+  while(increment < limit){
+    if((increment % 3) === 0 || (increment % 5) === 0) sum += increment
+    increment ++
+  }
+  return sum
+}
+
+findMultiples(1000)
